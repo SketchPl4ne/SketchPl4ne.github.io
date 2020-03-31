76f1eea402bfc197b78adf18814c9d68537e0f5a
sketchpl4ne <1609410364@qq.com>
2020-04-01 02:45:38 +0800

    Site updated: 2020-04-01 02:45:38

--- dist/music.js
+++ dist/music.js
@@ -28,6 +28,13 @@ const ap = new APlayer({
         url: 'https://sharefs.yun.kugou.com/202003312113/393fa800aebac39d58f3678e4ce3ba5f/G128/M09/10/05/IIcBAFqAzN-AIIU2AD5mqrBi2UA864.mp3',
         cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20190822/20190822134427228.jpg',
         lrc: '/musics/Lemon.lrc'
+      },
+      {
+        name: "LOSER",
+        artist: '米津玄師',
+        url: 'https://sharefs.yun.kugou.com/202004010242/63f77d36c4eb982e5f3e3e3f265f8ff5/G108/M0B/06/13/TJQEAFn5ZiqAXsuOADuM5IzBsC4090.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20190822/20190822134427228.jpg',
+        lrc: '/musics/LOSER.lrc'
       }
     ]
 });
