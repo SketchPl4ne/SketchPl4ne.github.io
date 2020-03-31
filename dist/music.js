4d6b22f0c505c516631a0177d7ea9cce90bbb84c
sketchpl4ne <1609410364@qq.com>
2020-04-01 03:22:01 +0800

    Site updated: 2020-04-01 03:22:01

--- dist/music.js
+++ dist/music.js
@@ -42,6 +42,20 @@ const ap = new APlayer({
         url: 'https://sharefs.yun.kugou.com/202004010249/b64bd12af718cd2810d46c3d945f463a/G011/M09/03/13/Sw0DAFUKv1KAOIRbADVsmVXW7Go154.mp3',
         cover: 'https://t1.picb.cc/uploads/2020/04/01/kSLVoG.jpg',
         lrc: '/musics/qn.lrc'
+      },
+      {
+        name: "なんでもないや",
+        artist: 'Akie秋绘',
+        url: 'https://sharefs.yun.kugou.com/202004010258/94ae1b3a243962c31f59587f8195b5b8/G099/M07/19/03/A4cBAFjaYe-AJW9QAFVvN4eGoug772.mp3',
+        cover: 'http://singerimg.kugou.com/uploadpic/pass/softhead/100/20190322/20190322020138221496.jpg',
+        lrc: '/musics/msmdbld.lrc'
+      },
+      {
+        name: "紅蓮華",
+        artist: 'Lisa',
+        url: 'https://sharefs.yun.kugou.com/202004010304/205140feb4c1aadf85d5b2f70aa248a3/G157/M07/14/03/fZQEAFytf0CAAJcUABX-NzHVU-A259.mp3',
+        cover: 'https://t1.picb.cc/uploads/2020/04/01/kSLZzc.png',
+        lrc: '/musics/hlh.lrc'
       }
     ]
 });
