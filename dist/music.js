dc33ae765ac46c7b37a7c7ebca8a6be53d511a88
sketchpl4ne <1609410364@qq.com>
2020-04-01 02:21:24 +0800

    Site updated: 2020-04-01 02:21:23

--- dist/music.js
+++ dist/music.js
@@ -11,16 +11,16 @@ const ap = new APlayer({
       {
         name: "Lose Yourself",
         artist: 'Eminem',
-        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_28488382&response=res&type=convert_url&',
-        //url: '/musics/LoseYourself.mp3',
+        //url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_28488382&response=res&type=convert_url&',
+        url: '/musics/LoseYourself.mp3',
         cover: 'https://t1.picb.cc/uploads/2019/09/26/gxyvj7.th.jpg',
         lrc: '/musics/LoseYourself.lrc'
       },
 	  {
 		name: "Radioactive",
         artist: 'Imagine Dragons',
-        //url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_3967148&response=res&type=convert_url&',
-        url: 'https://sharefs.yun.kugou.com/202003312113/393fa800aebac39d58f3678e4ce3ba5f/G128/M09/10/05/IIcBAFqAzN-AIIU2AD5mqrBi2UA864.mp3',
+        url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_3967148&response=res&type=convert_url&',
+        //url: 'https://sharefs.yun.kugou.com/202003312113/393fa800aebac39d58f3678e4ce3ba5f/G128/M09/10/05/IIcBAFqAzN-AIIU2AD5mqrBi2UA864.mp3',
         cover: 'https://t1.picb.cc/uploads/2019/09/26/gx2eC6.th.jpg',
         lrc: '/musics/Radioactive.lrc'
 	  },
